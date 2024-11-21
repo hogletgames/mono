a02f1eaa5f2a0b8d01e9ee218b49914cc4cb5425
Zorana Curkovic <zorana.curkovic@unity3d.com>
2024-11-21 19:41:58 +0000

    Tie ToolTip window to TreeView control to allow for ToolTip Window Destruction
    (cherry picked from commit 2ddb5a06232afe7f86fefd4dde198ddba40581c5)
    (cherry picked from commit d86e7a58d68c2ae5d97d58ba17d73339cf67527e)
    Signed-off-by: Zorana Curkovic <zorana.curkovic@unity3d.com>

--- mcs/class/System.Windows.Forms/System.Windows.Forms/TreeView.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/TreeView.cs
@@ -2229,14 +2229,19 @@ namespace System.Windows.Forms {
 			tooltip_currently_showing = null;
 		}
 
-		private ToolTip ToolTipWindow {
-			get {
+		private ToolTip ToolTipWindow
+		{
+			get
+			{
 				if (tooltip_window == null)
-					tooltip_window = new ToolTip ();
+				{
+					tooltip_window = new ToolTip(this);
+				}
 
 				return tooltip_window;
 			}
 		}
+
 		#endregion
 		
 		#endregion	// Internal & Private Methods and Properties
